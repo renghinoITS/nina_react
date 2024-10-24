09fb46a48202fae494f020ab8c90278a446c6395
francesco-renghi <allievo_renghi.f@itsumbria.it>
2024-10-24 17:23:39 +0200

    fixed useMQTT

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,8 +2,11 @@ import useMQTT from "../../hooks/useMQTT";
 import React, { useState } from "react";
 
 const App = () => {
-    const { messages } = useMQTT();
+    const { client, messages } = useMQTT({
+        ip: "10.10.15.85"
+    });
 
+    console.log(messages);
 
     return (
         <div>
--- src/hooks/useMQTT.tsx
+++ src/hooks/useMQTT.tsx
@@ -1,55 +1,51 @@
 import { useEffect, useState } from "react";
 import mqtt from "mqtt";
 
-// Definizione del tipo per il messaggio
-interface MqttMessage {
-	topic: string;
-	message: string;
+interface useMQTTProps {
+	ip?: string,
+	port?: number,
+	topic?: string
 }
 
-const useMQTT = () => {
+const useMQTT = ({ 
+	ip = "localhost",
+	port = 9001,
+	topic = "Test Topic"
+}: useMQTTProps) => {
 	const [client, setClient] = useState<mqtt.MqttClient | null>(null);
-	const [messages, setMessages] = useState<MqttMessage[]>([]);
+	const [messages, setMessages] = useState<string[]>([]);
 
 	useEffect(() => {
-		// Collegamento al broker Mosquitto tramite WebSocket
-		const mqttClient = mqtt.connect("ws://10.10.15.85:9001");
+		const mqttClient = mqtt.connect(`ws://${ip}:${port}`);
 
-		// Impostazione del client MQTT
 		mqttClient.on("connect", () => {
-		console.log("Connesso a Mosquitto tramite WebSocket");
+			console.log(`Connesso a Mosquitto tramite WebSocket all'indirizzo ws://${ip}:${port}`);
 
-		// Sottoscrizione a un topic MQTT
-			mqttClient.subscribe("Test Topic", (err) => {
-			if (!err) {
-				console.log("Sottoscritto a test/topic");
+			mqttClient.subscribe(topic, (err) => {
+				if (!err) {
+					console.log(`Sottoscritto al topic: ${topic}`);
 				}
 			});
 		});
 
-		// Gestione dei messaggi in arrivo
 		mqttClient.on("message", (topic, message) => {
-			// Usa TextDecoder per convertire l'Uint8Array in stringa
 			const decoder = new TextDecoder("utf-8");
 			const decodedString = decoder.decode(message);
 
-			console.log(decodedString);  // Output: "Test Payload"
-
 			setMessages((prevMessages) => [
-			...prevMessages,
-			{ topic, message: message.toString() }
+				...prevMessages,
+				decodedString
 			]);
 		});
 
 		setClient(mqttClient);
 
-		// Cleanup: disconnessione del client MQTT alla chiusura del componente
 		return () => {
 			mqttClient.end();
 		};
-	}, []);
+	}, [ip, port, topic]);
 
-	return { messages };
+	return { client, messages };
 };
 
 export default useMQTT;
