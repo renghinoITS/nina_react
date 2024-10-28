e3df5597fb9aa134ed11c64397665c42a409117c
AmedeoA <alessiamedeo25@gmail.com>
2024-10-28 15:43:43 +0100

    added component files

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import "./assets/css/index.css"
 
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import App from "./components/App/App"
+import Access
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
